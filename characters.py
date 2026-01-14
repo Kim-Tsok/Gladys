b7bbb6dd5d00ebeda4f41aa7232998de2d86fb0e
Kim-Tsok <appkim02@gmail.com>
2026-01-14 14:51:59 +0100

    Added the choice system

--- characters.py
+++ characters.py
@@ -5,6 +5,9 @@ init()
 # This is to make sure the character names are colored and to not clutter the main file
 characters = {
     "player" :f"{Fore.GREEN}You:{Style.RESET_ALL}",
+    "-player": "     ",
     "unknown" : f"{Fore.RED}Unknown:{Style.RESET_ALL}",
-    "person01" : f"{Fore.BLUE}Person01:{Style.RESET_ALL}"
+    "-unknown": "         ",
+    "person01" : f"{Fore.BLUE}Person01:{Style.RESET_ALL}",
+    "-person01" : "          "
 }
--- gladys.py
+++ gladys.py
@@ -27,25 +27,37 @@ def main():
 
     # Prologue
     input(f"{characters['unknown']} What is your motivation for all of this, why do you do this? ")
-    input("Make it easier on yourself, just give up. ")
-    input("Give up on this stupid dream of trying to beat me. ")
+    input(f"{characters['-unknown']}Make it easier on yourself, just give up. ")
+    input(f"{characters['-unknown']}Give up on this stupid dream of trying to beat me. ")
     input("")
-    input(f"{characters['person01']} Hey wake up! wakeup you idiot. ")
+    input(f"{characters['person01']} Hey wake up! wake up you idiot. ")
     input(f"{characters['player']} What happened? ")
     input(f"{characters['person01']} You were shaking and mumbling something in your sleep. ")
     input(f"{characters['player']} I had a dream. ")
     input(f"{characters['person01']} You saw him again, didn't you? ")
-    input(f"{characters['player']}: It was terrifying. ")
+    input(f"{characters['player']} It was terrifying. My movements felt heavy, my breathing was off. ")
+    input(f"{characters['-player']}I'm terrified ")
+    
+    while True:
+        choice = input(
+            f"{characters['person01']} Wanna spar cause we're weirdos? (y/n) "
+        ).strip().lower()
 
-    choice = input(
-        f"{characters['person01']} Wanna spar cause we're weirdos? (y/n) "
-    ).lower()
+        if choice == "y":
+            weapon = weapons["excalibur"].copy()
+            fight(player, weapon, armour, enemies["knight01"])
+            break
 
-    weapon = weapons["excalibur"].copy()
+        elif choice == "n":
+            input(f"{characters['-person01']}That's alright")
+            break
 
-    fight(player, weapon, armour, enemies["knight01"])
+        else:
+            print("Invalid input. Type y or n.")
 
-    input(" ")
+        # fight(player, weapon, armour, enemies["knight01"])
+        
+    input("")
     input("Didn't that feel rejuvinating. Doesn't it feel nice to stretch a bit")
 
 
