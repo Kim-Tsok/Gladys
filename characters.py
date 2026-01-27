938b6ce978063da3d3ff398a7566203968f67ea8
Kim-Tsok <appkim02@gmail.com>
2026-01-27 13:31:22 +0100

    Story update

--- characters.py
+++ characters.py
@@ -4,10 +4,10 @@ init()
 
 # This is to make sure the character names are colored and to not clutter the main file
 characters = {
-    "player" :f"{Fore.GREEN}You:{Style.RESET_ALL}",
+    "player" :f"{Fore.GREEN}You:{Style.RESET_ALL} ",
     "-player": "     ",
-    "unknown" : f"{Fore.RED}Unknown:{Style.RESET_ALL}",
+    "unknown" : f"{Fore.RED}Unknown:{Style.RESET_ALL} ",
     "-unknown": "         ",
-    "person01" : f"{Fore.BLUE}Person01:{Style.RESET_ALL}",
+    "person01" : f"{Fore.BLUE}Person01:{Style.RESET_ALL} ",
     "-person01" : "          "
 }
